b5851a9182e6c22b5600d46f1ce96ebd0969edc8
koert.goossens.bee <koertgoossens@hotmail.com>
2023-10-14 17:09:25 +0200

    service pattern added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 ﻿using CharacterAPI.Models;
+using CharacterAPI.Services.CharacterService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,29 +9,29 @@ namespace CharacterAPI.Controllers
 	[Route("api/[controller]")]
 	public class CharacterController : ControllerBase
 	{
-		private static List<Character> characters = new List<Character>
+		private readonly ICharacterService _characterService;
+
+		public CharacterController(ICharacterService characterService)
 		{
-			new Character { Id = 0, Name = "Frodo" },
-			new Character { Id = 1, Name = "Sam" }
-		};
+			_characterService = characterService;
+		}
 
 		[HttpGet("GetAll")]
 		public ActionResult<List<Character>> Get()
 		{
-			return Ok(characters);
+			return Ok(_characterService.GetAllCharacters());
 		}
 
 		[HttpGet("{id}")]
 		public ActionResult<Character> GetSingle(int id)
 		{
-			return Ok(characters.FirstOrDefault(c => c.Id == id));
+			return Ok(_characterService.GetCharacterById(id));
 		}
 
 		[HttpPost]
 		public ActionResult<List<Character>> AddCharacter(Character newCharacter)
 		{
-			characters.Add(newCharacter);
-			return Ok(characters);
+			return Ok(_characterService.AddCharacter(newCharacter));
 		}
 	}
 }
