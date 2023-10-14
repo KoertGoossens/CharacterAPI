8c0c1d3f9b167fbb8de46e1c510e90415f83c006
koert.goossens.bee <koertgoossens@hotmail.com>
2023-10-14 20:26:42 +0200

    implemented update and delete methods

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -43,5 +43,61 @@ namespace CharacterAPI.Services.CharacterService
 			serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
 			return serviceResponse;
 		}
+
+		public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+		{
+			var serviceResponse = new ServiceResponse<GetCharacterDto>();
+
+			try
+			{
+				var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+
+				if (character == null)
+				{
+					throw new Exception($"Character with Id '{updatedCharacter.Id}' not found.");
+				}
+
+				character.Name = updatedCharacter.Name;
+				character.HitPoints = updatedCharacter.HitPoints;
+				character.Strength = updatedCharacter.Strength;
+				character.Defense = updatedCharacter.Defense;
+				character.Intelligence = updatedCharacter.Intelligence;
+				character.Class = updatedCharacter.Class;
+
+				serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+			}
+			catch (Exception ex)
+			{
+				serviceResponse.Success = false;
+				serviceResponse.Message = ex.Message;
+			}
+
+			return serviceResponse;
+		}
+
+		public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+		{
+			var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+
+			try
+			{
+				var character = characters.FirstOrDefault(c => c.Id == id);
+
+				if (character == null)
+				{
+					throw new Exception($"Character with Id '{id}' not found.");
+				}
+
+				characters.Remove(character);
+				serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+			}
+			catch (Exception ex)
+			{
+				serviceResponse.Success = false;
+				serviceResponse.Message = ex.Message;
+			}
+
+			return serviceResponse;
+		}
 	}
 }
